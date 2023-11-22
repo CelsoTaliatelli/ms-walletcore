d4016ccf063e1157fce66df8895ed3b1be52f8ae
Celso Taliatelli <celsotaliatelli@hotmail.com>
2023-11-21 21:27:21 -0300

    uow

--- internal/gateway/transaction.go
+++ internal/gateway/transaction.go
@@ -4,4 +4,6 @@ import "github.com/CelsoTaliatelli/ms-walletcore/internal/entity"
 
 type TransactionGateway interface {
 	Create(transaction *entity.Transaction) error
+	findByID(id string) (*entity.Account, error)
+	UpdateBalance(entity *entity.Account) error
 }
